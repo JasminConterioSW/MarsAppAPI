d84dd4aabdfde4b0638fed24a6d2b49ef3bb9de4
JasCon <jasmin.conterio@softwire.com>
2024-09-23 14:44:05 +0100

    Setup working

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+const router = express.Router();
+router.get('/test', (req, res) => res.send('Hello world !'));
+app.use('/', router);
+
+router.get('/test2', (req, res) => res.send('The answer is 42!'));
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`Test backend is running on port ${port}`);
+});
